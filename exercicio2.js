7756a977104cc6c3e09e60d21c6f5e8e77fb85f2
kalancarvalho <kalancarvalho2021@gmail.com>
2024-08-30 14:27:51 -0300

    Add files via upload

---
+++ exercicio2.js
@@ -0,0 +1,15 @@
+// Crie uma variavel que receba e armazene a sua idade no tipo number
+
+let idade = 18
+
+// Crie uma outra variavel que receba o valor da sua primeira variavel, mas com o seu tipo alternado para string
+
+let idadeTipoString = String (idade)
+
+// Mostrar o valor e o tipo da primeira variavel
+
+console.log(idade, typeof idade)
+
+// Mostrar o valor e o tipo da segunda variavel
+
+console.log(idadeTipoString, typeof idadeTipoString)
