69bbd43a4a28fbdbde8b4521a2dd57a8b58844a8
kalancarvalho <kalancarvalho2021@gmail.com>
2024-09-02 16:56:24 -0300

    Add files via upload

---
+++ exercicios3.js
@@ -0,0 +1,12 @@
+
+
+//
+let nome = "Kalan"
+let anoDeNascimento = 2006
+let anoAtual = 2024
+let idade = anoAtual - anoDeNascimento
+let maiorDeIdade = (idade >= 18)
+ console.log("Nome:", nome)
+console.log( "Idade:", idade)
+console.log("É maior de idade?", maiorDeIdade)
+console.log("Em 2050", nome, "terá:", 2050 - anoDeNascimento)
